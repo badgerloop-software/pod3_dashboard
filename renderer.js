a9b9337057969e455ffd27ed3f32688af0ae6970
Ezra Boley <eboley@wisc.edu>
2018-01-09 12:30:43 -0600

    Finished refactoring the frame design and changed the label on the state box

--- renderer.js
+++ renderer.js
@@ -4,12 +4,8 @@ const electron = require('electron');
 const request = require('request');
 const stateControl = require('./stateManager');
 const frameStyle = require('./frameStyle');
-const myButton = document.getElementById("close");
-const minimizer = document.getElementById('minimize');
-const maximizer = document.getElementById('maximize');
 
 
-const brand = document.getElementById('brand');
 const os = require("os");
 let data = document.getElementsByClassName('data');
 const port = 3000;
@@ -17,45 +13,8 @@ const port = 3000;
 
 
 
-myButton.addEventListener('click', (err) => {
-    electron.remote.getCurrentWindow().close();
-});
-
-minimizer.addEventListener('click', (err) => {
-    electron.remote.getCurrentWindow().minimize();
-});
 
 
-let previousSize;
-
-maximizer.addEventListener('click', (err) => {
-    if (electron.remote.getCurrentWindow().isMaximized()){
-        electron.remote.getCurrentWindow().setSize(previousSize[0], previousSize[1]);
-        electron.remote.getCurrentWindow().center();
-        maximizer.innerHTML = '&#128470';
-    } else {
-        previousSize = electron.remote.getCurrentWindow().getSize();
-        electron.remote.getCurrentWindow().maximize();
-        maximizer.innerHTML = '&#128470;';
-    }
-
-
-});
-
-
-let newWindow;
-brand.addEventListener('click', () => {
-    newWindow = new electron.remote.BrowserWindow({
-        width: 600,
-        height: 600,
-        show: false,
-        icon: __dirname + '/assets/icons/png/64x64.png'
-    })
-
-    newWindow.loadURL('http://badgerloop.com');
-    newWindow.on('ready-to-show', () => {newWindow.show()})
-    newWindow.on('closed', () => {newWindow = null})
-})
 
 
 let id = 0;
